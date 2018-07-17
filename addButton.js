7ee0f8318a3bbda8ec82daa41f740b5251577800
Josh Ruud <joshruud@gmail.com>
2018-07-17 10:24:40 -0400

    indentation

--- addButton.js
+++ addButton.js
@@ -5,16 +5,12 @@ function addButton () {
 
  	if (audio) {
 
-  $("<a class='x-btn' id='downloadAudio' download>Download Audio</a>").insertAfter('.x-audio');
+    $("<a class='x-btn' id='downloadAudio' download>Download Audio</a>").insertAfter('.x-audio');
 
- $('#downloadAudio').attr("href", audio);
+    $('#downloadAudio').attr("href", audio);
   }
 }
 
 if ($('article').attr("class", "type-post")) {
     setTimeout( addButton, 1000);
   }
-
-
-
-  
